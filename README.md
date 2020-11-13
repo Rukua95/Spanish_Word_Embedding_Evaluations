cb91390b3fedfdb96424d2e6bdfd89c557759f20
Thibo <tswysen95@gmail.com>
2020-11-12 23:23:04 -0300

    Addition of info in readme file

--- README.md
+++ README.md
@@ -1,1 +1,126 @@
-# Evaluacion de Word Embeddings para Español
+# Evaluation of Spanish Word Embeddings
+
+
+<!-- TABLE OF CONTENTS -->
+## Table of Contents
+
+* [About the Project](#about-the-project)
+* [Evaluations and Datasets](#evaluation-and-datasets)
+  * [Word Similarity](#word-similarity)
+  * [Word Analogy](#word-analogy)
+  * [Outlier Detection](#outlier-detection)
+  * [Cross-match](#cross-match)
+  * [Text Classification](#text-classification)
+* [Usage](#usage)
+* [Results](#results)
+  * [Word Similarity](#word-similarity-results)
+  * [Word Analogy](#word-similarity-results)
+  * [Outlier Detection](#word-similarity-results)
+  * [Cross-match](#word-similarity-results)
+  * [Text Classification](#word-similarity-results)
+* [Reference](#reference)
+
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+This is a tool for the validation of word embeddings for the Spanish language. This tool use differents types 
+of evaluation methods: Word Similarity, Word Analogy, Outlier Detection, Cross-match and Text Classification.
+
+<!-- EVALUATION AND DATASETS -->
+## Evaluations and Datasets
+
+### Word Similiarity
+
+
+
+### Word Analogy
+
+### Outlier Detection
+
+### Cross-match
+
+### Text Classification
+
+
+<!-- USAGE EXAMPLE -->
+## Usage
+
+
+
+```
+python IntrinsecEvaluation.py -f <PATH> -s -a -o -c <PATH>
+```
+
+```
+python ExtrinsecEvaluation.py -f <PATH> -lstm -mean
+```
+
+```
+
+```
+
+
+<!-- RESULTS -->
+## Results
+
+This evaluation tool has been used to compare differents word embeddings. The results obtained are presented below as 
+the mean results of every dataset.
+
+### Word Similiarity
+
+| Word Embeddings   |  Pearson | Spearman | Kendall |
+| :---------------- | :--: | :--: | :--: |
+| Fasttext-SUC(M)   | 0.66 | 0.68 | 0.52 |
+| Fasttext-SUC(L)   | 0.68 | 0.70 | 0.54 |
+| Fasttext-SUC(NL)  | asdf | asdf | asdf |
+| Fasttext-SBWC     | asdf | asdf | asdf |
+| Fasttext-Wiki     | asdf | asdf | asdf |
+| GloVe-SBWC        | asdf | asdf | asdf |
+| W2V-SBWC          | asdf | asdf | asdf |
+| BETO              | asdf | asdf | asdf |
+
+
+### Word Analogy
+
+| Word Embeddings   | GA Semantic | GA Sintactic | CATS Semantic | CATS Sintactic |
+| :---------------- | :--: | :--: | :--: | :--: |
+| Fasttext-SUC(M)   | 0.66 | 0.68 | 0.52 | asdf |
+| Fasttext-SUC(L)   | 0.68 | 0.70 | 0.54 | asdf |
+| Fasttext-SUC(NL)  | asdf | asdf | asdf | asdf |
+| Fasttext-SBWC     | asdf | asdf | asdf | asdf |
+| Fasttext-Wiki     | asdf | asdf | asdf | asdf |
+| GloVe-SBWC        | asdf | asdf | asdf | asdf |
+| W2V-SBWC          | asdf | asdf | asdf | asdf |
+
+
+### Outlier Detection
+
+| Word Embeddings   | Accuracy | OPP |
+| :---------------- | :--: | :--: |
+| Fasttext-SUC(M)   | 0.84 | 0.63 |
+| Fasttext-SUC(L)   | **0.86** | **0.66** |
+| Fasttext-SUC(NL)  | 0.85 | 0.65 |
+| Fasttext-SBWC     | 0.85 | 0.65 |
+| Fasttext-Wiki     | 0.84 | 0.64 |
+| GloVe-SBWC        | 0.82 | 0.62 |
+| W2V-SBWC          | 0.78 | 0.53 |
+| BETO              | 0.72 | 0.44 |
+
+
+### Cross-match
+
+| Word Embeddings | Fasttext-SBWC | GloVe-SBWC | W2V-SBWC |
+| :-------------- | :--: | :--: | :--: |
+| Fasttext-SBWC   | \--- | 7.81x10<sup>-28</sup> | 1.11x10<sup>-30</sup> |
+| GloVe-SBWC      | 7.81x10<sup>-28</sup> | \--- | 6.7x10<sup>-28</sup> |
+| W2V-SBWC        | 1.11x10<sup>-30</sup> | 6.7x10<sup>-28</sup> | \--- |
+
+
+### Text Classification
+
+
+
+<!-- REFERENCE -->
+## Reference
---
+++ Testing/IntrinsecEvaluation.py
@@ -0,0 +1,323 @@
+#!/usr/bin/env python
+
+import os
+import io
+import sys
+import argparse
+
+from pathlib import Path
+from gensim.models.keyedvectors import KeyedVectors
+
+from SimilarityTest import SimilarityTestClass
+from AnalogyTest import AnalogyTestClass
+from OutlierDetectionTest import OutlierDetectionTestClass
+from CrossMatchTest import CrossMatchTestClass
+
+
+class Embedding:
+    def __init__(self, embedding):
+        self._embedding = embedding
+
+    def __getitem__(self, item):
+        return self._embedding[item]
+
+    def __contains__(self, item):
+        return (item in self._embedding)
+
+    # Entrega tamaño de los vectores dentro del embedding
+    def vectorSize(self):
+        return self._embedding.vector_size
+
+    # Entrega lista de todas las palabras en el vocabulario del embedding.
+    # Las palabras estan en el mismo orden que la lista de vectores
+    def getWordList(self):
+        return self._embedding.index2word
+
+    # Entrega lista de todos los vectores en el embedding.
+    # Los vectores estan en el mismo orden que la lista de palabras
+    def getVectors(self):
+        self._embedding.init_sims()
+        return self._embedding.vectors_norm
+
+
+def getWordEmbedding(embedding_path):
+    print("> Loading word embedding...")
+    try:
+        word_embedding = KeyedVectors.load_word2vec_format(embedding_path, limit=None)
+    except:
+        raise Exception("Is not posible to load the word embedding: " + str(embedding_path))
+    print("  Done loading word embedding")
+
+    return word_embedding
+
+def multipleEmbeddingEval(args):
+    # Get embedding
+    try:
+        word_embeddings_directory = Path(args.embedding_directory)
+        embeddings_files_name = os.listdir(word_embeddings_directory)
+    except:
+        raise Exception("Problems at reading path directory with embeddings: " + str(word_embeddings_directory))
+
+    # Get data for evaluations
+    similarity_datasets = args.similarity_datasets
+    analogy_datasets = args.analogy_datasets
+    outlier = args.outlier_detection
+    cross_match_emb = args.cross_match_emb
+
+    verbose = args.verbose
+
+    try:
+        if cross_match_emb != None:
+            cross_match_emb = Path(args.cross_match_emb)
+    except:
+        raise Exception("Cross-match second embedding path is invalid: " + str(cross_match_emb))
+
+    if similarity_datasets != None:
+        similarity_test = SimilarityTestClass(use_intersect_dataset=True, datasets=similarity_datasets)
+        similarity_test.resetIntersectDataset()
+
+    if analogy_datasets != None:
+        analogy_test = AnalogyTestClass(use_intersect_dataset=True, datasets=analogy_datasets, metrics=["3CosMul"])
+        analogy_test.resetIntersectDataset()
+
+    if outlier:
+        outlier_detection_test = OutlierDetectionTestClass(use_intersect_dataset=True)
+        outlier_detection_test.resetIntersectDataset()
+
+    for embedding_file in embeddings_files_name:
+        print("> Intersecting dataset with", (word_embeddings_directory / embedding_file).stem, "vocabulary...")
+        embedding = getWordEmbedding(word_embeddings_directory / embedding_file)
+        word_embedding = Embedding(embedding)
+
+        if similarity_datasets != None:
+            similarity_test.intersectDataset(word_embedding)
+
+        if analogy_datasets != None:
+            analogy_test.intersectDataset(word_embedding)
+
+        if outlier:
+            outlier_detection_test.intersectDataset(word_embedding)
+
+        print("  Done intersecting")
+        del word_embedding
+
+    for embedding_file in embeddings_files_name:
+        embedding_path = word_embeddings_directory / embedding_file
+
+        embedding = getWordEmbedding(embedding_path)
+        word_embedding = Embedding(embedding)
+        word_embedding_name = embedding_path.stem
+
+        if similarity_datasets != None:
+            print("> Word similarity test")
+            results = similarity_test.evaluateWordVector(word_embedding_name, word_embedding)
+            print("  Evaluation complete\n")
+
+            if verbose:
+                print("> Results & Info")
+                for r in results:
+                    print(r)
+
+                print("\n")
+
+        if analogy_datasets != None:
+            print("> Word analogy test")
+            results = analogy_test.evaluateWordVector(word_embedding_name, word_embedding)
+            print("  Evaluation complete")
+
+            if verbose:
+                print("> Results & Info")
+                for r in results:
+                    print(r)
+
+                print("\n")
+
+        if outlier:
+            print("> Outlier detection test")
+            results = outlier_detection_test.evaluateWordVector(word_embedding_name, word_embedding)
+            print("  Evaluation complete")
+
+            if verbose:
+                print("> Results & Info")
+                for r in results:
+                    print(r)
+
+                print("\n")
+
+        if cross_match_emb != None:
+            print("> Cross-match test")
+            word_embedding2 = getWordEmbedding(cross_match_emb)
+            word_embedding_name2 = cross_match_emb.stem
+
+            cross_match_test = CrossMatchTestClass()
+            results = cross_match_test.crossMatchTest(word_embedding, word_embedding_name, word_embedding2,
+                                                      word_embedding_name2)
+            print("  Evaluation complete")
+
+            if verbose:
+                print("> Results & Info")
+                for r in results:
+                    print(r)
+
+                print("\n")
+
+
+def singleEmbeddingEval(args):
+    # Info from args
+    word_embedding_path = args.embedding_file
+    word_embeddings_directory = args.embedding_directory
+
+    similarity_datasets = args.similarity_datasets
+    analogy_datasets = args.analogy_datasets
+    outlier = args.outlier_detection
+    cross_match_emb = args.cross_match_emb
+
+    verbose = args.verbose
+
+    try:
+        if cross_match_emb != None:
+            cross_match_emb = Path(args.cross_match_emb)
+    except:
+        raise Exception("Cross-match second embedding path is invalid")
+
+    word_embedding_path = Path(word_embedding_path)
+    embedding = getWordEmbedding(word_embedding_path)
+    word_embedding = Embedding(embedding)
+    word_embedding_name = word_embedding_path.stem
+
+    if similarity_datasets != None:
+        print("> Word similarity test")
+        similarity_test = SimilarityTestClass(datasets=similarity_datasets)
+        results = similarity_test.evaluateWordVector(word_embedding_name, word_embedding)
+        print("  Evaluation complete\n")
+
+        if verbose:
+            print("> Results & Info")
+            for r in results:
+                print(r)
+
+            print("\n")
+
+    if analogy_datasets != None:
+        print("> Word analogy test")
+        analogy_test = AnalogyTestClass(datasets=analogy_datasets, metrics=["3CosMul"])
+        results = analogy_test.evaluateWordVector(word_embedding_name, word_embedding)
+        print("  Evaluation complete")
+
+        if verbose:
+            print("> Results & Info")
+            for r in results:
+                print(r)
+
+            print("\n")
+
+    if outlier:
+        print("> Outlier detection test")
+        outlier_detection_test = OutlierDetectionTestClass()
+        results = outlier_detection_test.evaluateWordVector(word_embedding_name, word_embedding)
+        print("  Evaluation complete")
+
+        if verbose:
+            print("> Results & Info")
+            for r in results:
+                print(r)
+
+            print("\n")
+
+    if cross_match_emb != None:
+        print("> Cross-match test")
+        word_embedding2 = getWordEmbedding(cross_match_emb)
+        word_embedding_name2 = cross_match_emb.stem
+
+        cross_match_test = CrossMatchTestClass()
+        results = cross_match_test.crossMatchTest(word_embedding, word_embedding_name, word_embedding2,
+                                                  word_embedding_name2)
+        print("  Evaluation complete")
+
+        if verbose:
+            print("> Results & Info")
+            for r in results:
+                print(r)
+
+            print("\n")
+
+def main():
+
+    # Parser
+    my_parser = argparse.ArgumentParser(description='Word embedding evaluation program. Word embeddings are loaded '
+                                                    'using load_word2vec_format from KeyedVectors in gensim library. '
+                                                    'Results are saved in folder Results, under the same name as the '
+                                                    'file containing the embedding.')
+
+    my_group = my_parser.add_mutually_exclusive_group(required=True)
+
+    # Argumentos
+    my_group.add_argument('-d',
+                          metavar='PATH',
+                          type=str,
+                          action='store',
+                          dest='embedding_directory',
+                          help='Path to directory containing word embeddings to evaluate.')
+
+    my_group.add_argument('-f',
+                          metavar='PATH',
+                          type=str,
+                          action='store',
+                          dest='embedding_file',
+                          help='Path to word embedding to evaluate.')
+
+    my_parser.add_argument('-a',
+                           action='store',
+                           metavar='DATASET',
+                           type=str,
+                           nargs='*',
+                           dest='analogy_datasets',
+                           help='Evaluate using word analogy. If no dataset is given, the evaluation use all '
+                                'dataset avalible. By default, only 3CosMul is used during evaluation.')
+
+    my_parser.add_argument('-s',
+                           action='store',
+                           metavar='DATASET',
+                           type=str,
+                           nargs='*',
+                           dest='similarity_datasets',
+                           help='Evaluate using semantic similarity. If no dataset is given, the evaluation '
+                                'use all dataset avalible. Pearson r, Spearman rho and Kendall tau are used during '
+                                'evaluation.')
+
+    my_parser.add_argument('-o',
+                           action='store_true',
+                           dest='outlier_detection',
+                           help='Evaluate using outlier detection.')
+
+    my_parser.add_argument('-c',
+                           metavar='PATH',
+                           action='store',
+                           type=str,
+                           dest='cross_match_emb',
+                           help='Compare first word embedding with the embedding from the given path. In case '
+                                'first path given is a folder, cross-match evaluation is done with all embeddings '
+                                'from folder.')
+
+    my_parser.add_argument('-v',
+                           '--verbose',
+                           dest='verbose',
+                           action='store_true',
+                           help='Directly show results from evaluations and related info.')
+
+    args = my_parser.parse_args()
+    #print(vars(args))
+
+    pass
+
+    # Info from args
+    word_embedding_path = args.embedding_file
+    word_embeddings_directory = args.embedding_directory
+
+    if word_embedding_path != None:
+        singleEmbeddingEval(args)
+    elif word_embeddings_directory != None:
+        multipleEmbeddingEval(args)
+
+if __name__ == '__main__':
+    main()
